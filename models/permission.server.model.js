8e4f1301b6cd0dbd109e55e6869a21f9e6b69a2a
pronein <adam.dot.schrader@gmail.com>
2016-06-05 19:47:08 -0400

    Update models

--- models/permission.server.model.js
+++ models/permission.server.model.js
@@ -1,10 +1,30 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+/*
+ * Model Definition
+ */
+
 var PermissionSchema = new Schema({
   name: String,
   description: String,
-  category: String
+  category: {type: String, lowercase: true}
 });
 
+/*
+ * Indexes
+ */
+
+/*
+ * Statics
+ */
+
+/*
+ * Register Model
+ */
+
 mongoose.model('Permission', PermissionSchema);
+
+/*
+ * Internals
+ */
--- models/role.server.model.js
+++ models/role.server.model.js
@@ -1,9 +1,30 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+/*
+ * Model Definition
+ */
+
 var RoleSchema = new Schema({
   name: String,
+  description: String,
   permissions: [{type: Schema.Types.ObjectId, ref: 'Permission'}]
 });
 
+/*
+ * Indexes
+ */
+
+/*
+ * Statics
+ */
+
+/*
+ * Register Model
+ */
+
 mongoose.model('Role', RoleSchema);
+
+/*
+ * Internals
+ */

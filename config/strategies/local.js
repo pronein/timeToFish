f33ef778e653acde81bc226ff9e5239835475366
pronein <adam.dot.schrader@gmail.com>
2016-06-05 02:50:39 -0400

    Use sha256 digest to hash passwords and create salts for user accounts
    Pull auth route logic into server controller
    
    Session controller (ng) now informs server of requestor (user || browser)

--- config/strategies/local.js
+++ config/strategies/local.js
@@ -16,31 +16,32 @@ module.exports = function () {
   });
 
   return new LocalStrategy(
-      function (username, password, done) {
-        User.findOne({
-          $or: [{username: username}, {email: username}]
-        }, function (err, user) {
-          if (err) {
-            log.error({err: err}, 'Error while trying to find the user for authentication');
-            return done(err);
-          }
+    function (username, password, done) {
+      User.findOne({
+        $or: [{username: username}, {email: username}]
+      }, function (err, user) {
+        if (err) {
+          log.error({err: err}, 'Error while trying to find the user for authentication');
+          return done(err);
+        }
 
-          if (!user) {
-            log.debug('User does not exist [' + username + ']');
-            return done(null, false);
-          }
+        if (!user) {
+          log.debug('User does not exist [' + username + ']');
+          return done(null, false);
+        }
 
-          if (validateUser(user, password))
-            done(null, user);
-          else {
-            log.debug('Could not validate user [' + username + ']');
-            done(null, false);
-          }
-        });
+        if (_tryAuthenticateUser(user, password))
+          done(null, user);
+        else {
+          log.debug('Could not authenticate user [' + username + ']');
+          done(null, false);
+        }
       });
+    });
 };
 
-function validateUser(user, password) {
-  var hash = hasher.hash(password);
-  return user !== null && hash === user.passwordHash;
+function _tryAuthenticateUser(user, clearTextPassword) {
+  var generatedHash = hasher.generateHash(clearTextPassword, user.password.salt);
+
+  return generatedHash === user.password.hash;
 }

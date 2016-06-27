17753f83b31bee55d18ca8a1f72d1a06f87bfadf
pronein <adam.dot.schrader@gmail.com>
2016-06-27 17:25:29 -0400

    Add actual controller for swagger definition of /api/menus/{key} [TTF-27] Complete

--- api/controllers/menu-items.server.controller.js
+++ api/controllers/menu-items.server.controller.js
@@ -3,33 +3,33 @@ var MenuItem = mongoose.model('MenuItem');
 var log = require('../../auxiliary/logger');
 
 module.exports = {
-  getAll: getAll
+  getAll: getAll,
+  getByKey: getByKey
 };
 
 function getAll(req, res, next) {
   MenuItem.find({})
     .populate('permissions')
     .populate('parent')
-    .exec(function(err, menuItems){
-      if(err)
+    .exec(function (err, menuItems) {
+      if (err)
         _handleError(err, res, 'Failed to retrieve menu items.');
 
-      menuItems.sort(function(left, right) {
-        //check keys for equality
-        if (left.key !== right.key)
-          return left.key.localeCompare(right.key);
+      _doMenuItemSort(menuItems);
 
-        //keys are equal, check parents
-        if (!left.parent && right.parent)
-          return -1;
-        else if (!right.parent && left.parent)
-          return 1;
-        else if (!left.parent && right.parent)
-          return 0;
+      res.json(menuItems);
+    });
+}
 
-        //parents are equal, do alphabetical names
-        return left.name.localeCompare(right.name);
-      });
+function getByKey(req, res, next) {
+  MenuItem.find({key: req.swagger.params.key.value})
+    .populate('permissions')
+    .populate('parent')
+    .exec(function(err, menuItems) {
+      if(err)
+        _handleError(err, res, 'Failed to retrieve menu items by key.');
+
+      _doMenuItemSort(menuItems);
 
       res.json(menuItems);
     });
@@ -39,4 +39,23 @@ function _handleError(err, res, msg) {
   log.error({err: err}, msg);
 
   res.status(500).json({msg: msg});
+}
+
+function _doMenuItemSort(menuItems) {
+  menuItems.sort(function (left, right) {
+    //check keys for equality
+    if (left.key !== right.key)
+      return left.key.localeCompare(right.key);
+
+    //keys are equal, check parents
+    if (!left.parent && right.parent)
+      return -1;
+    else if (!right.parent && left.parent)
+      return 1;
+    else if (!left.parent && right.parent)
+      return 0;
+
+    //parents are equal, do alphabetical names
+    return left.name.localeCompare(right.name);
+  });
 }

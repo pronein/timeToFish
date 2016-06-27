2a8ffdd05639abbebc2343e8dfa249afdcaf6ae4
pronein <adam.dot.schrader@gmail.com>
2016-06-27 17:57:48 -0400

    [TTF-28] Add swagger definition for /api/menus/{id}

--- api/mocks/menu-items.server.controller.js
+++ api/mocks/menu-items.server.controller.js
@@ -1,11 +1,39 @@
 module.exports = {
   getAll: getAll,
-  getByKey: getByKey
+  getById: getById
 };
 
 function getAll(req, res, next) {
+  var menuItems = [{
+    key: 'a_key',
+    name: 'some name here',
+    sequence: 10,
+    description: 'something described here',
+    parent: null,
+    state: 'menus.keyView',
+    permissions: []
+  }, {
+    key: 'b_key',
+    name: 'different name',
+    sequence: 20,
+    description: 'other description',
+    parent: null,
+    state: 'menus.keyView_2',
+    permissions: ['perm_1']
+  }];
+
+  if(req.swagger.params.key.value)
+    res.json({menuItems: menuItems.filter(function(mItem){
+      return mItem.key === req.swagger.params.key.value;
+    })});
+  else
+    res.json({menuItems: menuItems});
+}
+
+function getById(req, res, next) {
   res.json({
-    menuItems: [{
+    menuItem: {
+      id: req.swagger.params.id.value,
       key: 'the_key',
       name: 'a name to display',
       sequence: 10,
@@ -13,31 +41,6 @@ function getAll(req, res, next) {
       parent: null,
       state: 'menus.view',
       permissions: ['perm_1', 'perm_2', 'perm_3']
-    }]
+    }
   });
-}
-
-function getByKey(req, res, next) {
-  if (req.swagger.params.key.value === 'a_key')
-    res.json({
-      menuItems: [{
-        key: 'a_key',
-        name: 'some name here',
-        sequence: 10,
-        description: 'something described here',
-        parent: null,
-        state: 'menus.keyView',
-        permissions: []
-      }, {
-        key: 'a_key',
-        name: 'different name',
-        sequence: 20,
-        description: 'other description',
-        parent: null,
-        state: 'menus.keyView_2',
-        permissions: ['perm_1']
-      }]
-    });
-  else
-    res.json({menuItems: []});
 }

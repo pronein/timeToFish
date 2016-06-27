0585056a66dc8145da29b3341eb98ae66ab2a631
pronein <adam.dot.schrader@gmail.com>
2016-06-27 00:28:30 -0400

    [TTF-24] Add menu-item mongoose model.

--- models/menu-item.server.model.js
+++ models/menu-item.server.model.js
@@ -1,14 +1,46 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+/*
+ * Model Definition
+ */
+
 var MenuItemSchema = new Schema({
-  menuId: Number,
-  permissionRequired: {type: Schema.Types.ObjectId, ref: 'Permission'},
-  name: {
-    state: String,
-    display: String
-  },
-  owner: String
-}, {collection: 'menuItems'});
+  name: {type: String, required: true},
+  description: String,
+  parent: {type: Schema.Types.ObjectId, ref: 'MenuItem', default: null},
+  permissions: [{type: Schema.Types.ObjectId, ref: 'Permission'}],
+  key: {type: String, required: true, lowercase: true, trim: true, set: replaceSpaces},
+  sequence: {type: Number, default: 10, required: true, set: integerOnly},
+  state: {type: String, trim: true}
+});
+
+/*
+ * Indexes
+ */
+
+MenuItemSchema.index({key: 1});
+
+/*
+ * Register Model
+ */
 
 mongoose.model('MenuItem', MenuItemSchema);
+
+/*
+ * Internals
+ */
+
+function integerOnly(val) {
+  if (typeof val === 'number') {
+    return parseInt(val);
+  }
+
+  return val;
+}
+
+function replaceSpaces(val) {
+  if (typeof val === 'string') {
+    return val.replace(/\s*/g, '_');
+  }
+}

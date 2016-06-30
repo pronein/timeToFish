85220f945e63c52a90a457e2413664116d69a35c
pronein <adam.dot.schrader@gmail.com>
2016-06-29 22:08:46 -0400

    [TTF-25] Add swagger mock controller for POST /api/menus

--- api/mocks/menu-items.server.controller.js
+++ api/mocks/menu-items.server.controller.js
@@ -1,10 +1,12 @@
 module.exports = {
   getAll: getAll,
-  getById: getById
+  getById: getById,
+  create: create
 };
 
-function getAll(req, res, next) {
-  var menuItems = [{
+var _internalCollection = [
+  {
+    id: '5753bfabe9279fe82a226725',
     key: 'a_key',
     name: 'some name here',
     sequence: 10,
@@ -13,6 +15,7 @@ function getAll(req, res, next) {
     state: 'menus.keyView',
     permissions: []
   }, {
+    id: '5753bfabe9279fe82a226726',
     key: 'b_key',
     name: 'different name',
     sequence: 20,
@@ -20,27 +23,64 @@ function getAll(req, res, next) {
     parent: null,
     state: 'menus.keyView_2',
     permissions: ['perm_1']
-  }];
+  }, {
+    id: '5753bfabe9279fe82a226727',
+    key: 'b_key',
+    name: 'a name to display',
+    sequence: 10,
+    description: 'some description',
+    parent: {
+      id: '5753bfabe9279fe82a226726',
+      key: 'b_key',
+      name: 'different name',
+      sequence: 20,
+      description: 'other description',
+      parent: null,
+      state: 'menus.keyView_2',
+      permissions: ['perm_1']
+    },
+    state: 'menus.view',
+    permissions: ['perm_2', 'perm_3']
+  }
+];
 
+function getAll(req, res, next) {
   if(req.swagger.params.key.value)
-    res.json({menuItems: menuItems.filter(function(mItem){
+    res.json({menuItems: _internalCollection.filter(function(mItem){
       return mItem.key === req.swagger.params.key.value;
     })});
   else
-    res.json({menuItems: menuItems});
+    res.json({menuItems: _internalCollection});
 }
 
 function getById(req, res, next) {
   res.json({
+    menuItem: _internalCollection.filter(function(mItem){
+      return mItem.id === req.swagger.params.id.value;
+    })
+  });
+}
+
+function create(req, res, next) {
+  var parent = _internalCollection.filter(function(mItem){
+    return mItem.id === req.body.parent;
+  });
+
+  if (parent.length)
+    parent = parent[0];
+  else
+    parent = null;
+
+  res.json({
     menuItem: {
-      id: req.swagger.params.id.value,
-      key: 'the_key',
-      name: 'a name to display',
-      sequence: 10,
-      description: 'some description',
-      parent: null,
-      state: 'menus.view',
-      permissions: ['perm_1', 'perm_2', 'perm_3']
+      id: '5753bfabe9279fe82a226724',
+      key: req.body.key,
+      name: req.body.name,
+      sequence: req.body.sequence,
+      description: req.body.description,
+      parent: parent,
+      state: req.body.state,
+      permissions: req.body.permissions || []
     }
   });
 }
